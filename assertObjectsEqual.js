d84aa3f8ab7f9bd098e585fd530a41bb4c30ce4c
Eugene Helland <eugenehelland@mail.com>
2022-03-10 15:20:29 -0700

    Created map.js. Implemented a custom version of the built-in map function for javascript.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,6 +1,6 @@
 const eqArrays = (arr1, arr2) => {
   // If parameters are of different length return false
-  if (arr1.length !== arr2.length) return false;
+  if (arr1.length != arr2.length) return false;
 
   for (let i = 0; i < arr1.length; i++) {
     // If any value in arr1 does not match the value at the same index in arr2
