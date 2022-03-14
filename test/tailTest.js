ededba149b9e5c6883f931559b01c54245258481
Eugene Helland <eugenehelland@mail.com>
2022-03-14 10:50:30 -0600

    Completed tailTest.js.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,11 @@
 const tail = require('../tail.js');
-const assertEqual = require('../assertArraysEqual.js');
+const assertArrayEqual = require('../assertArrayEqual.js');
 
 
 const testArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const testArray2 = [];
+const testArray2 = [[]];
 const testArray3 = ['I', 'am', 'the', 'Batman'];
 
-assertEqual(tail(testArray1), [2, 3, 4, 5, 6, 7, 8, 9]);
-assertEqual(tail(testArray2), undefined);
-assertEqual(tail(testArray3), ['am', 'the', 'Batman']);
+assertArrayEqual(tail(testArray1), [2, 3, 4, 5, 6, 7, 8, 9]);
+assertArrayEqual(tail(testArray2), []);
+assertArrayEqual(tail(testArray3), ['am', 'the', 'Batman']);
